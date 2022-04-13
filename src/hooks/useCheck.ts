e13edbaa1b879b9bd530e5eeb5892db3e0b937c7
Your Name <you@example.com>
2022-04-13 10:45:56 +0800

    check

--- src/hooks/useCheck.ts
+++ src/hooks/useCheck.ts
@@ -3,6 +3,7 @@ import { useLayoutEffect, useMemo, useState } from "react";
 function useCheck<L>(initList: L[] | undefined,): {
 	list: L[],
 	check: (index: number) => void,
+	check_all: (bool: boolean) => void,
 	total_checked: L[],
 } {
 	const [list, setList] = useState<L[]>([]);
@@ -20,10 +21,18 @@ function useCheck<L>(initList: L[] | undefined,): {
 		}
 		setList($list)
 	}
+	const check_all = (bool: boolean) => {
+		const $list = JSON.parse(JSON.stringify(list));
+		$list.forEach((element: any) => {
+			element.checked = bool
+		});
+		setList($list)
+	}
 
 	return {
 		list,
 		check: check_item,
+		check_all,
 		total_checked,
 	}
 }
