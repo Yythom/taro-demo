5ccbc5ce1329b3e77f7133d27ee35205e5af790d
虎虎 <1078013818@qq.com>
2022-05-10 14:20:14 +0800

    scen

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,24 +1,30 @@
-import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
+import Taro from '@tarojs/taro'
 import './index.scss'
 
-export default class Index extends Component {
-
-  componentWillMount () { }
-
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
-
-  render () {
-    return (
-      <View className='index'>
-        <Text>Hello world!</Text>
-      </View>
-    )
+function sceneDecode(scene: string) {
+  if (scene === undefined)
+    return {};
+  let _scene = decodeURIComponent(scene),
+    params = _scene.split(','),
+    data = {};
+  for (let i in params) {
+    var val = params[i].split(':');
+    val.length > 0 && val[0] && (data[val[0]] = val[1] || null)
   }
+  return data;
 }
+
+function Index() {
+  // scene=a%3A43%2Cluck_id%3A43%2Cperiod_id%3A43
+  const scene = Taro.getCurrentInstance().router?.params?.scene
+  console.log(sceneDecode(scene as string));
+
+  return (
+    <View className='index'>
+      <Text>Hello world!</Text>
+    </View>
+  )
+}
+
+export default Index
